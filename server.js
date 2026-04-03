3b6f5076248ca6f0eb803982ba70d5bb9ad9acf4
jaysonFullStackDev <jayson.a.quisquirin@gmail.com>
2026-04-03 15:52:46 +0800

    Connect backend to frontend

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://my-port-folio-two-ashy.vercel.app/",
+  }),
+);
 app.use(express.json());
 
 const contactLimiter = rateLimit({
